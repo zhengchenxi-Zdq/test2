cba3511102d7977cfa571144436c982266edc292
unknown <2607975205@qq.com>
2026-07-24 02:40:29 +0000

    first commit

---
+++ src/main/java/org/example/cicd/controller/MessageController.java
@@ -0,0 +1,4 @@
+package org.example.cicd.controller;
+
+public class MessageController {
+}
---
+++ src/test/java/org/example/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
